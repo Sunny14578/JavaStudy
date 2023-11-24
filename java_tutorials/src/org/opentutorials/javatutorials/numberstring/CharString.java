da870dbb689c35a08f875936c402ab342440d7bf
Sunny14578 <imsun0101@naver.com>
2023-11-24 21:36:24 +0900

    first commit.

---
+++ java_tutorials/src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("생활");
+		System.out.println("egoin \"said Welcome programming wolrd\"");
+	}
+
+}
---
+++ java_tutorials/src/org/opentutorials/javatutorials/numberstring/IntergerDemo.java
@@ -0,0 +1,19 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class IntergerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a = 1;
+		System.out.println(a+1);
+		
+		a = 2;
+		System.out.println(a+1);
+		
+//		double (실수) 
+		double b;
+		b = 1.1;
+	}
+
+}
